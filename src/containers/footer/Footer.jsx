735ccbb0d860ce5d4c1340ec09ce5ba2794d4032
Michal Novák <michal.novak@seacomp.cz>
2023-11-22 15:24:34 +0100

    footer WIP

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import logo from "../../assets/logo.svg";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future?
+        </h1>
+      </div>
+
+      <div className="gpt3__footer-button">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="gpt3 logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
